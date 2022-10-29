a87ce9c69a4db80a5d37688e6e41d59b82713ad0
tomson-george <me.tomsongeorge@gmail.com>
2022-10-29 14:08:27 -0400

    Optimising file path

--- application.py
+++ application.py
@@ -23,10 +23,11 @@ def home():
         # Getting uploaded file name
         img_filename = secure_filename(file.filename)
         # file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),application.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], img_filename))
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], img_filename)
+        file.save(filepath)
         # Storing uploaded file path in flask session
         uploaded_img_path = os.path.join(application.config['UPLOAD_FOLDER'], img_filename)
-        return render_template("uploaded_successfully.html",user_image = uploaded_img_path)
+        return render_template("uploaded_successfully.html",user_image = filepath)
     return render_template('index.html', form=form)
 
 if __name__ == '__main__':

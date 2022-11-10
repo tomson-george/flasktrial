63ec4cf90460ecc81b6d5df1eb53a34461470911
Tomson George <84787705+tomson-george@users.noreply.github.com>
2022-11-10 11:33:44 -0500

    Update application.py

--- application.py
+++ application.py
@@ -22,10 +22,10 @@ def home():
         file = form.file.data # First grab the file
         # Getting uploaded file name
         img_filename = secure_filename(file.filename)
-        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),application.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
+        # file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),application.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
         # file.save(os.path.join(application.config['UPLOAD_FOLDER'], secure_filename(file.filename)))
-        # filepath = os.path.join(app.config['UPLOAD_FOLDER'], img_filename)
-        # file.save(filepath)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], img_filename)
+        file.save(filepath)
         return render_template("uploaded_successfully.html",user_image = filepath)
         return  "File uploaded succesfully"
     return render_template('index.html', form=form)

7858c92a70d491a4c94472496070cb774157c0e4
Tomson George <84787705+tomson-george@users.noreply.github.com>
2022-11-10 11:37:49 -0500

    Update application.py

--- application.py
+++ application.py
@@ -24,9 +24,9 @@ def home():
         img_filename = secure_filename(file.filename)
         # file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),application.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
         # file.save(os.path.join(application.config['UPLOAD_FOLDER'], secure_filename(file.filename)))
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'], img_filename)
-        file.save(filepath)
-        return render_template("uploaded_successfully.html",user_image = filepath)
+        #filepath = os.path.join(app.config['UPLOAD_FOLDER'], img_filename)
+        file.save(img_filename)
+        return render_template("uploaded_successfully.html",user_image = img_filename)
         return  "File uploaded succesfully"
     return render_template('index.html', form=form)
 
